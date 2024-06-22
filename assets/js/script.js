51b239635c420101ba28fa67f66d18e9e4ae9737
012marcal <marcal.jobsdev@gmail.com>
2024-06-22 02:59:59 -0300

    inicio 2

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+}
+
+texto = document.querySelector('.escrever')
+typeWriter(texto);
